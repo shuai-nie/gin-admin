140d0a7e53cf0221b4ed8e6b7e605d1252cca187
Dave <yesheng35@126.com>
2025-12-26 21:22:37 +0800

    更新

--- pkg/cachex/cache.go
+++ pkg/cachex/cache.go
@@ -2,6 +2,7 @@ package cachex
 
 import (
 	"context"
+	"fmt"
 	"time"
 
 	"github.com/patrickmn/go-cache"
@@ -9,6 +10,12 @@ import (
 
 type Cacher interface {
 	Set(ctx context.Context, ns, key, value string, expiration ...time.Duration) error
+	Get(ctx context.Context, ns, key string) (string, bool, error)
+	GetAndDelete(ctx context.Context, ns, key string) (string, bool, error)
+	Exists(ctx context.Context, ns, key string) (bool, error)
+	Delete(ctx context.Context, ns, key string) (bool, error)
+	Iterator(ctx context.Context, ns string, fn func(ctx context.Context, key, value string) bool) error
+	Close(ctx context.Context) error
 }
 
 var defaultDelimiter = ":"
@@ -18,3 +25,37 @@ type options struct {
 }
 
 type Option func(*options)
+
+func WithDelimiter(delimiter string) Option {
+	return func(o *options) {
+		o.Delimiter = delimiter
+	}
+}
+
+type MemoryConfig struct {
+	CleanupInterval time.Duration
+}
+
+func NewMemoryCache(cfg MemoryConfig, opts ...Option) Cacher {
+	defaultOpts := &options{
+		Delimiter: defaultDelimiter,
+	}
+
+	for _, o := range opts {
+		o(defaultOpts)
+	}
+
+	return &memCache{
+		opts:  defaultOpts,
+		cache: cache.New(0, cfg.CleanupInterval),
+	}
+}
+
+type memCache struct {
+	opts  *options
+	cache *cache.Cache
+}
+
+func (a *memCache) getKey(ns, key string) string {
+	return fmt.Sprintf("%s%s%s", ns, a.opts.Delimiter, key)
+}
