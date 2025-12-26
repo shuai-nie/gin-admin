3f4e62ba13826681e4f49c993852f14165cc2a76
Dave <yesheng35@126.com>
2025-12-26 21:45:32 +0800

    33

--- pkg/cachex/cache.go
+++ pkg/cachex/cache.go
@@ -14,7 +14,7 @@ type Cacher interface {
 	Get(ctx context.Context, ns, key string) (string, bool, error)
 	GetAndDelete(ctx context.Context, ns, key string) (string, bool, error)
 	Exists(ctx context.Context, ns, key string) (bool, error)
-	Delete(ctx context.Context, ns, key string) (bool, error)
+	Delete(ctx context.Context, ns, key string) error
 	Iterator(ctx context.Context, ns string, fn func(ctx context.Context, key, value string) bool) error
 	Close(ctx context.Context) error
 }
