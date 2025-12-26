0749208f92be1c0b593c8d340ccce7a8069017ca
Dave <yesheng35@126.com>
2025-12-26 21:44:01 +0800

    11

--- pkg/cachex/cache.go
+++ pkg/cachex/cache.go
@@ -3,6 +3,7 @@ package cachex
 import (
 	"context"
 	"fmt"
+	"strings"
 	"time"
 
 	"github.com/patrickmn/go-cache"
@@ -58,4 +59,60 @@ type memCache struct {
 
 func (a *memCache) getKey(ns, key string) string {
 	return fmt.Sprintf("%s%s%s", ns, a.opts.Delimiter, key)
+}
+
+func (a *memCache) Set(ctx context.Context, ns, key, value string, expiration ...time.Duration) error {
+	var exp time.Duration
+	if len(expiration) > 0 {
+		exp = expiration[0]
+	}
+
+	a.cache.Set(a.getKey(ns, key), value, exp)
+	return nil
+}
+
+func (a *memCache) Get(ctx context.Context, ns, key string) (string, bool, error) {
+	val, ok := a.cache.Get(a.getKey(ns, key))
+	if !ok {
+		return "", false, nil
+	}
+	return val.(string), ok, nil
+}
+
+func (a *memCache) Exists(ctx context.Context, ns, key string) (bool, error) {
+	_, ok := a.cache.Get(a.getKey(ns, key))
+	return ok, nil
+}
+
+func (a *memCache) Delete(ctx context.Context, ns, key string) error {
+	a.cache.Delete(a.getKey(ns, key))
+	return nil
+}
+
+func (a *memCache) GetAndDelete(ctx context.Context, ns, key string) (string, bool, error) {
+	value, ok, err := a.Get(ctx, ns, key)
+	if err != nil {
+		return "", false, err
+	} else if !ok {
+		return "", false, nil
+	}
+
+	a.cache.Delete(a.getKey(ns, key))
+	return value, true, nil
+}
+
+func (a *memCache) Iterator(ctx context.Context, ns string, fn func(ctx context.Context, key, value string) bool) error {
+	for k, v := range a.cache.Items() {
+		if strings.HasPrefix(k, a.getKey(ns, "")) {
+			if !fn(ctx, strings.TrimPrefix(k, a.getKey(ns, "")), v.Object.(string)) {
+				break
+			}
+		}
+	}
+	return nil
+}
+
+func (a *memCache) Close(ctx context.Context) error {
+	a.cache.Flush()
+	return nil
 }
