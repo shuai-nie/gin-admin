8948ac5421f5cf96b67ed2162319e28ec975ecf6
Dave <yesheng35@126.com>
2025-12-30 20:31:42 +0800

    添加 error 错误

--- pkg/errors/errors.go
+++ pkg/errors/errors.go
@@ -1,6 +1,11 @@
 package errors
 
 import (
+	"fmt"
+	"gin-admin/pkg/encoding/json"
+	"net/http"
+	"sync"
+
 	"github.com/pkg/errors"
 )
 
@@ -13,10 +18,16 @@ var (
 )
 
 const (
-	DefaultBadRequestID   = "bad_request"
-	DefaultUnauthorizedID = "unauthorized"
-	DefaultForbiddenID    = "forbidden"
-	DefaultNotFoundID     = "not_found"
+	DefaultBadRequestID            = "bad_request"
+	DefaultUnauthorizedID          = "unauthorized"
+	DefaultForbiddenID             = "forbidden"
+	DefaultNotFoundID              = "not_found"
+	DefaultMethodNotAllowedID      = "method_not_allowed"
+	DefaultTooManyRequestsID       = "too_many_requests"
+	DefaultRequestEntityTooLargeID = "request_entity_too_large"
+	DefaultInternalServerErrorID   = "internal_server_error"
+	DefaultConflictID              = "conflict"
+	DefaultRequestTimeoutID        = "request_timeout"
 )
 
 type Error struct {
@@ -24,4 +35,215 @@ type Error struct {
 	Code   int32
 	Detail string
 	Status string
+}
+
+func (e *Error) Error() string {
+	b, _ := json.Marshal(e)
+	return string(b)
+}
+
+func New(id, detail string, code int32) error {
+	return &Error{
+		ID:     id,
+		Code:   code,
+		Detail: detail,
+		Status: http.StatusText(int(code)),
+	}
+}
+
+func Parse(err string) *Error {
+	e := new(Error)
+	errr := json.Unmarshal([]byte(err), e)
+	if errr != nil {
+		e.Detail = err
+	}
+	return e
+}
+
+func BadRequest(id, format string, a ...interface{}) error {
+	if id == "" {
+		id = DefaultBadRequestID
+	}
+	return &Error{
+		ID:     id,
+		Code:   http.StatusBadRequest,
+		Detail: fmt.Sprintf(format, a...),
+		Status: http.StatusText(http.StatusBadRequest),
+	}
+}
+
+func Unauthorized(id, format string, a ...interface{}) error {
+	if id == "" {
+		id = DefaultUnauthorizedID
+	}
+	return &Error{
+		ID:     id,
+		Code:   http.StatusUnauthorized,
+		Detail: fmt.Sprintf(format, a...),
+		Status: http.StatusText(http.StatusUnauthorized),
+	}
+}
+
+func Forbidden(id, format string, a ...interface{}) error {
+	if id == "" {
+		id = DefaultForbiddenID
+	}
+	return &Error{
+		ID:     id,
+		Code:   http.StatusForbidden,
+		Detail: fmt.Sprintf(format, a...),
+		Status: http.StatusText(http.StatusForbidden),
+	}
+}
+
+func NotFound(id, format string, a ...interface{}) error {
+	if id == "" {
+		id = DefaultNotFoundID
+	}
+	return &Error{
+		ID:     id,
+		Code:   http.StatusNotFound,
+		Detail: fmt.Sprintf(format, a...),
+		Status: http.StatusText(http.StatusNotFound),
+	}
+}
+
+func MethodNotAllowed(id, format string, a ...interface{}) error {
+	if id == "" {
+		id = DefaultMethodNotAllowedID
+	}
+	return &Error{
+		ID:     id,
+		Code:   http.StatusMethodNotAllowed,
+		Detail: fmt.Sprintf(format, a...),
+		Status: http.StatusText(http.StatusMethodNotAllowed),
+	}
+}
+
+func TooManyRequests(id, format string, a ...interface{}) error {
+	if id == "" {
+		id = DefaultTooManyRequestsID
+	}
+	return &Error{
+		ID:     id,
+		Code:   http.StatusTooManyRequests,
+		Detail: fmt.Sprintf(format, a...),
+		Status: http.StatusText(http.StatusTooManyRequests),
+	}
+}
+
+func Timeout(id, format string, a ...interface{}) error {
+	if id == "" {
+		id = DefaultRequestTimeoutID
+	}
+	return &Error{
+		ID:     id,
+		Code:   http.StatusRequestTimeout,
+		Detail: fmt.Sprintf(format, a...),
+		Status: http.StatusText(http.StatusRequestTimeout),
+	}
+}
+
+func Conflict(id, format string, a ...interface{}) error {
+	if id == "" {
+		id = DefaultConflictID
+	}
+	return &Error{
+		ID:     id,
+		Code:   http.StatusConflict,
+		Detail: fmt.Sprintf(format, a...),
+		Status: http.StatusText(http.StatusConflict),
+	}
+}
+
+func RequestEntityTooLarge(id, format string, a ...interface{}) error {
+	if id == "" {
+		id = DefaultRequestEntityTooLargeID
+	}
+	return &Error{
+		ID:     id,
+		Code:   http.StatusRequestEntityTooLarge,
+		Detail: fmt.Sprintf(format, a...),
+		Status: http.StatusText(http.StatusRequestEntityTooLarge),
+	}
+}
+
+func InternalServiceError(id, format string, a ...interface{}) error {
+	if id == "" {
+		id = DefaultInternalServerErrorID
+	}
+	return &Error{
+		ID:     id,
+		Code:   http.StatusInternalServerError,
+		Detail: fmt.Sprintf(format, a...),
+		Status: http.StatusText(http.StatusInternalServerError),
+	}
+}
+
+func Equal(err1 error, err2 error) bool {
+	verr1, ok1 := err1.(*Error)
+	verr2, ok2 := err2.(*Error)
+	if ok1 != ok2 {
+		return false
+	}
+	if !ok1 {
+		return err1 == err2
+	}
+	if verr1.Code != verr2.Code {
+		return false
+	}
+	return true
+}
+
+func FromError(err error) *Error {
+	if err == nil {
+		return nil
+	}
+
+	if verr, ok := err.(*Error); ok && verr != nil {
+		return verr
+	}
+	return Parse(err.Error())
+}
+
+func As(err error) (*Error, bool) {
+	if err == nil {
+		return nil, false
+	}
+	var merr *Error
+	if errors.As(err, &merr) {
+		return merr, true
+	}
+	return nil, false
+}
+
+type MultiError struct {
+	lock   *sync.Mutex
+	Errors []error
+}
+
+func NewMultiError() *MultiError {
+	return &MultiError{
+		lock:   new(sync.Mutex),
+		Errors: make([]error, 0),
+	}
+}
+
+func (e *MultiError) Append(err error) {
+	e.Errors = append(e.Errors, err)
+}
+
+func (e *MultiError) AppendWithLock(err error) {
+	e.lock.Lock()
+	defer e.lock.Unlock()
+	e.Append(err)
+}
+
+func (e *MultiError) HasError() bool {
+	return len(e.Errors) > 0
+}
+
+func (e *MultiError) Error() string {
+	b, _ := json.Marshal(e)
+	return string(b)
 }
