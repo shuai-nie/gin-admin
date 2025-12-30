7690cce8fd9da03d25e82e6b1d8215c850358929
Dave <yesheng35@126.com>
2025-12-30 21:39:30 +0800

    更新

--- pkg/util/db.go
+++ pkg/util/db.go
@@ -41,7 +41,84 @@ func wrapQueryOptions(db *gorm.DB, opts QueryOptions) *gorm.DB {
 		db = db.Omit(opts.OmitFields...)
 	}
 	if len(opts.OrderFields) > 0 {
-		db = db.Order(opts.OrderFields...)
+		db = db.Order(opts.OrderFields.ToSQL())
 	}
 	return db
+}
+
+func WrapPageQuery(ctx context.Context, db *gorm.DB, pp PaginationParam, opts QueryOptions, out interface{}) (*PaginationResult, error) {
+	if pp.OnlyCount {
+		var count int64
+		err := db.Count(&count).Error
+		if err != nil {
+			return nil, err
+		}
+		return &PaginationResult{
+			Total: count,
+		}, nil
+	} else {
+		pageSize := pp.PageSize
+		if pageSize > 0 {
+			db = db.Limit(pageSize)
+		}
+
+		db = wrapQueryOptions(db, opts)
+		err := db.Find(out).Error
+		return nil, err
+	}
+
+	total, err := FindPage(ctx, db, pp, opts, out)
+	if err != nil {
+		return nil, err
+	}
+	return &PaginationResult{
+		Total:    total,
+		Current:  pp.Current,
+		PageSize: pp.PageSize,
+	}, nil
+}
+
+func FindPage(ctx context.Context, db *gorm.DB, pp PaginationParam, opts QueryOptions, out interface{}) (int64, error) {
+	db = db.WithContext(ctx)
+	var count int64
+	err := db.Count(&count).Error
+	if err != nil {
+		return 0, err
+	} else if count == 0 {
+		return count, nil
+	}
+
+	current, pageSize := pp.Current, pp.PageSize
+	if current > 0 && pageSize > 0 {
+		db = db.Offset((current - 1) * pageSize).Limit(pageSize)
+	} else if pageSize > 0 {
+		db = db.Limit(pageSize)
+	}
+
+	db = wrapQueryOptions(db, opts)
+	err = db.Find(out).Error
+	return count, err
+}
+
+func FindOne(ctx context.Context, db *gorm.DB, opts QueryOptions, out interface{}) (bool, error) {
+	db = db.WithContext(ctx)
+	db = wrapQueryOptions(db, opts)
+	result := db.First(out)
+	if err := result.Error; err != nil {
+		if err == gorm.ErrRecordNotFound {
+			return false, nil
+		}
+		return false, err
+	}
+	return true, nil
+}
+
+func Exists(ctx context.Context, db *gorm.DB) (bool, error) {
+	db = db.WithContext(ctx)
+	var count int64
+	result := db.Count(&count)
+	if err := result.Error; err != nil {
+		return false, err
+	}
+	return count > 0, nil
 }
