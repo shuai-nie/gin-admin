d5e095363c7221ecad342f953e0804ef14d46acf
Dave <yesheng35@126.com>
2025-12-31 21:53:54 +0800

    111

--- pkg/crypto/aes/aes.go
+++ pkg/crypto/aes/aes.go
@@ -1,5 +1,13 @@
 package aes
 
+import "bytes"
+
 var (
 	SecretKey = []byte("2985BCFDB5FE43129843DB59825F8647")
 )
+
+func PKCS5Padding(plaintext []byte, blockSize int) []byte {
+	padding := blockSize - len(plaintext)%blockSize
+	padtext := bytes.Repeat([]byte{byte(padding)}, padding)
+	return append(plaintext, padtext...)
+}
